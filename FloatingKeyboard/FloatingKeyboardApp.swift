5145d8da808b13030e4928d29a9dfac6a5f59e03
Festo Magembe <festomanolo@gmail.com>
2026-03-09 00:02:24 +0300

    Initial Commit

---
+++ FloatingKeyboard/FloatingKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingKeyboardApp.swift
+//  FloatingKeyboard
+//
+//  Created by festomanolo on 09/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct FloatingKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
